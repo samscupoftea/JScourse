24ef1a62ae29908fc4a0c7ccc097be0bf7e16f77
sam hockaday <samuel.hockaday@gmail.com>
2023-03-28 15:44:50 +0100

    added

---
+++ script.js
@@ -0,0 +1,124 @@
+
+/*
+40 + 8 + 23 - 10;
+console.log(40 + 8 + 23 - 10);
+
+console.log('Jonas');
+console.log(23);
+
+let firstName = "Matilda";
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let myFirstJob = 'Programmer';
+let myCurrentJob = 'Teacher';
+console.log(myFirstJob);
+console.log(myCurrentJob);
+*/
+
+
+
+
+/*
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+
+console.log(typeof true);
+
+
+
+const age = 15;
+
+
+if(age >= 18) {
+    console.log('Sarah can start driving license');
+} else {
+    const yearsLeft = 18 - age;
+    console.log(`Sarah is too young. Wait another ${yearsLeft} years :)`);
+}
+
+/*
+
+const country = 67.33
+const continent = 56.55
+console.log(country);
+console.log(continent);
+console.log(`The population of my country is ${country} million.`);
+console.log(`The population of the continent is ${continent} million.`);
+
+
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+// console.log(typeof true);
+console.log(typeof javascriptIsFun);
+//console.log(typeof 23);
+//console.log(typeof 'Jonas');
+
+javascriptIsFun = 'YES!';
+console.log(typeof javascriptIsFun);
+
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+
+year = 1991;
+console.log(typeof year);
+
+console.log(typeof null);
+
+
+let age = 30; 
+age = 31;
+
+const birthYear = 1990;
+//birthYear = 1990 
+
+
+//Math operators 
+const now = 2037;
+const ageJonas = now - 1991; 
+const ageSarah = now - 2018;
+console.log(ageJonas, ageSarah);
+
+console.log(ageJonas * 2, ageJonas / 10, 2** 3)
+// 2 ** 3 means 2 to the power of 3 = 2 * 2 * 2
+
+const firstName = 'Jonas'; 
+const lastName = 'Schedtmann';
+console.log(firstName + ' ' + lastName);
+
+//Assignment operators 
+
+let x = 10 + 5; 
+x += 10; // x = x + 10 = 25 
+x *= 4; // x = x * 4 = 100 
+x++; // x = x +1
+x--; // x = x - 1 
+x--;
+console.log(x);
+
+//Comparison operators 
+console.log(ageJonas > ageSarah); 
+// > GREATER THAN, < LESS THAN, >= GREATER THAN OR EQUAL TO, < LESS THAN OR EQUAL TO.
+console.log(ageSarah >= 18);
+
+ const isFullAge = ageSarah >= 18;
+
+ console.log(now - 1991 > now - 2018);
+
+ */
+
+
+ const now = 2037;
+const ageJonas = now - 1991; 
+const ageSarah = now - 2018;
+
+console.log(now - 1991 > now - 2018);
