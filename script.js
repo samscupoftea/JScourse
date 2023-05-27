7be54f12fb3d6f27ea490612349976e34aefb64d
sam hockaday <samuel.hockaday@gmail.com>
2023-05-27 11:09:42 +0100

    JS Coding challenge

--- script.js
+++ script.js
@@ -754,7 +754,12 @@ if (friends.includes('Peter')) {
 //1, WRITE FUNCTION CALLED 'calcTip'
 
 
+//Here is the funciton called calcTip. 
+// We use a ternary operator ? as it is one simple peramteter. 
 const calcTip = function (bill) {
     return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
-
 }
+
+const bills = [125, 555, 44];
+const tip = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
+
