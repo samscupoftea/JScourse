d59272912764d4bf4087f80eefd0cb915a7381a0
sam hockaday <samuel.hockaday@gmail.com>
2023-06-05 12:23:48 +0100

    Break with number

--- script.js
+++ script.js
@@ -874,3 +874,9 @@ for (let i = 0; i < jonas.length; i++) {
     console.log(jonas[i], typeof jonas[i]);
 }
 
+console.log('-----  BREAK WITH NUMBER -----')
+for (let i = 0; i < jonas.length; i++) {
+    if (typeof jonas[i] === 'number') continue;
+    console.log(jonas[i], typeof jonas[i]);
+}
+
