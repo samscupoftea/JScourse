e968ae3fd8a0627a49754faa3753432e4177a14d
sam hockaday <samuel.hockaday@gmail.com>
2023-06-05 12:54:02 +0100

    Loops within loops using let

--- script.js
+++ script.js
@@ -895,3 +895,12 @@ const jonas = [
 for (let i = jonas.length - 1; i >= 0; i--) {
     console.log(i, jonas[i]);
 }
+
+for (let exercise = 1; exercise < 4; exercise++) {
+    console.log(`--------Starting excercise ${exercise}`);
+
+    for (let rep = 1; rep < 6; rep++) {
+        console.log(`Lifting weight competition ${rep}`);
+    }
+
+}
