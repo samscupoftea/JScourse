1f2a9784357824c9d5bd2dcb0409bbf663e3316d
sam hockaday <samuel.hockaday@gmail.com>
2023-07-29 15:02:58 +0100

    Splice, Reverse, Concat

--- script.js
+++ script.js
@@ -1256,3 +1256,22 @@ let arr = ['a', 'b', 'c', 'd', 'e'];
 console.log(arr.length);
 console.log(arr[1]);
 console.log(arr.slice(2));
+console.log(arr.slice(-2));
+console.log(arr.slice(1, -2));
+console.log(arr.slice());
+console.log(...arr);
+
+//SPLICE 
+
+console.log(arr.splice(2));
+console.log(arr.splice(1, 2));
+console.log(...arr);
+
+//REVERSE 
+
+let arr2 = ['f', 'g', 'h', 'i', 'j'];
+console.log(arr2.reverse());
+
+//CONCAT
+const letters = arr.concat(arr2);
+console.log(letters);
